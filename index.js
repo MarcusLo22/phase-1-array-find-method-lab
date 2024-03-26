789b0d456bf0de490fe47ac1e7ba32880921e95b
Marcus Lo <marcusklo1010@gmail.com>
2024-03-25 21:47:43 -0600

    array find method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(records) {
+    // Find the first record with a result of "W" (win)
+    const winRecord = records.find(record => record.result === "W");
+  
+    // If a win record is found, return the year
+    if (winRecord) {
+      return winRecord.year;
+    }
+  
+    // If no win record is found, return undefined
+    return undefined;
+  }
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
